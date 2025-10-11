a5ce1d80a8f1ac50249cd304309ec7765ded6274
josephbnh <josephbenhamou7@gmail.com>
2025-10-11 23:00:48 +0100

    Added new endpoints and commands/queries

--- Auditory.API/Controllers/StreamController.cs
+++ Auditory.API/Controllers/StreamController.cs
@@ -24,4 +24,47 @@ public class StreamController(IMediator mediator) : ControllerBase
             return StatusCode(500, "Internal server error");
         }
     }
+
+    [HttpGet("{streamId:guid}")]
+    public async Task<IActionResult> GetStreamById(Guid streamId)
+    {
+        try
+        {
+            if (streamId == Guid.Empty)
+                return BadRequest("Stream ID cannot be empty");
+
+            var query = new Application.Queries.GetStreamByIdQuery(streamId);
+            var stream = await _mediator.Send(query);
+            if (stream is null)
+                return NotFound("Stream not found");
+
+            return Ok(stream);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            return StatusCode(500, "Internal server error");
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateStream([FromBody] Application.Commands.CreateStreamCommand command)
+    {
+        try
+        {
+            if (command is null)
+                return BadRequest("Command cannot be null");
+            
+            var createdStream = await _mediator.Send(command);
+            if(createdStream is null)
+                return BadRequest("Stream could not be created");
+            
+            return CreatedAtAction(nameof(GetStreamById), new { streamId = createdStream.Id, createdStream });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            return StatusCode(500, "Internal server error");
+        }
+    }
 }
---
+++ Auditory.Application/Handlers/CreateStreamCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Auditory.Application.Commands;
+using Auditory.Domain.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Stream = Auditory.Domain.Entities.Stream;
+
+namespace Auditory.Application.Handlers;
+
+public class CreateStreamCommandHandler(IStreamRepository streamRepository, ILogger<CreateStreamCommandHandler> logger)
+    : IRequestHandler<CreateStreamCommand, Stream>
+{
+    private readonly IStreamRepository _streamRepository = streamRepository ?? throw new ArgumentNullException(nameof(streamRepository));
+    private readonly ILogger<CreateStreamCommandHandler> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    public async Task<Stream> Handle(CreateStreamCommand request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Creating a new stream");
+        try
+        {
+            var newStream = new Stream
+            {
+                UserName = request.userName,
+                Platform = request.platform,
+                MsPlayed = request.msPlayed,
+                ConnCountry = request.connCountry,
+                TrackName = request.trackName,
+                ArtistName = request.artistName,
+                AlbumName = request.albumName,
+                SpotifyTrackUri = request.spotifyTrackUri,
+                Timestamp = request.timestamp
+            };
+
+            await _streamRepository.AddStreamAsync(newStream);
+            
+            _logger.LogInformation("Stream created with ID: {StreamId}", newStream.Id);
+            
+            return newStream;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("An error occurred while creating a new stream: {ErrorMessage}", ex.Message);
+            throw; // Re-throw the exception after logging it
+        }
+    }
+}
